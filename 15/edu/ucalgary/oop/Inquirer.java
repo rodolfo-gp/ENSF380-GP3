32942c3f15bafcb2ffe5301524e5ee4d2ed93c92
Inko-z <rodolfoandresgp@hotmail.com>
2024-02-15 23:37:21 -0700

    Added inquire changes

--- 15/edu/ucalgary/oop/Inquirer.java
+++ 15/edu/ucalgary/oop/Inquirer.java
@@ -1,5 +1,35 @@
 package edu.ucalgary.oop;
 
+// Inquirer Class:
+
 public class Inquirer {
-    
+
+// Private Data Members: 
+
+private String FIRST_NAME ;
+private String LAST_NAME ;
+private String INFO ;
+private String SERVICES_PHONE ;
+
+
+// Constructor: 
+
+public Inquirer(String FIRST_NAME, String LAST_NAME, String SERVICES_PHONE, String INFO) {
+
+
+
+}
+
+
+// Getters:
+
+public String getFirstName() {return this.FIRST_NAME ;}
+public String getLastName() {return this.LAST_NAME ;}
+public String getInfo() {return this.INFO ;}
+//public String get
+
+
+// Constructor: 
+
+
 }
