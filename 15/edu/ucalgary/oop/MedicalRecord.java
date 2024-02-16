a88b3b4b9cd0649eea86cfa180f3efd61d9bb158
Jaimal Sahota <jaimal.sahota@ucalgary.ca>
2024-02-15 21:54:04 -0700

    Implemented MedicalRecord.java

--- 15/edu/ucalgary/oop/MedicalRecord.java
+++ 15/edu/ucalgary/oop/MedicalRecord.java
@@ -6,7 +6,48 @@ Group 15
 package edu.calgary.oop;
 
 public class MedicalRecord {
-	
+	private Location location;
+    private String treatmentDetails;
+    private String dateOfTreatment;
+    private static final String REGEX = "\\d{4}-\\d{2}-\\d{2}"
+	private static final Pattern PATTERN = Pattern.compile(REGEX);
 
+    // Constructors
+    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) {
+		if (location == null || treatmentDetails == null || dateOfTreatment == null) {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+        else {
+            this.location = location;
+            this.treatmentDetails = treatmentDetails;
+            this.dateOfTreatment = dateOfTreatment;
+        }
+    }
 
+    // getters
+    public Location getLocation() {
+        return location;
+    }
+    public String getTreatmentDetails() {
+        return treatmentDetails;
+    }
+    public Stirng getDateOfTreatment() {
+        return dateOfTreatment;
+    }
+
+    // setters
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    public void setTreatmentDetails(String treatmentDetails) {
+        this.treatmentDetails = treatmentDetails;
+    }
+    public void setDateOfTreatment(String dateOfTreatment) {
+        if (PATTERN.matcher(dateOfTreatment).matches()) {
+            this.dateOfTreatment = dateOfTreatment;
+        }
+        else {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+    }
 }
